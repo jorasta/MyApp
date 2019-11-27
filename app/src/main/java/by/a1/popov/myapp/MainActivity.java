4952c0342d112655e953bef1503e55e80f17ce9a
Evgeniy Popov <Evgeniy_P@MAIN.VELCOM.BY>
2019-11-27 20:48:33 +0300

    Initial commit

---
+++ app/src/main/java/by/a1/popov/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package by.a1.popov.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
