21289b76a1d619c3bbf968eed1b29d3ae85ae70c
Yevgeni Popov <e.popov@a1.by>
2019-12-05 20:20:01 +0300

    WebView Task All Done without arrow

--- webbrowser_task3/src/main/java/by/a1/popov/webbrowser_task3/MainActivity.java
+++ webbrowser_task3/src/main/java/by/a1/popov/webbrowser_task3/MainActivity.java
@@ -2,13 +2,63 @@ package by.a1.popov.webbrowser_task3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_opn_brwsr).setOnClickListener(this);
+        findViewById(R.id.btn_prev_page).setOnClickListener(this);
+
+        mWebView = findViewById(R.id.webView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        mWebView.loadUrl("https://google.com");
+
     }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.btn_opn_brwsr:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mWebView.getUrl()));
+                startActivity(browserIntent);
+                break;
+            case R.id.btn_prev_page:
+
+                    mWebView.goBack();
+
+                break;
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
