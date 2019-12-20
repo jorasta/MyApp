385818ae874be2798ea738a05e0614eb6f83312d
Yevgeni Popov <e.popov@a1.by>
2019-12-20 23:17:49 +0300

    Homework4 Task with WebView - done Task with CustomView - done Change OnToucheEvent from activity to CustomView Class

--- homework4app/src/main/java/by/a1/popov/homework4app/MainActivity.java
+++ homework4app/src/main/java/by/a1/popov/homework4app/MainActivity.java
@@ -1,14 +1,42 @@
 package by.a1.popov.homework4app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.opn_phonebook).setOnClickListener(this);
+        findViewById(R.id.opn_customview).setOnClickListener(this);
+        findViewById(R.id.opn_webpage).setOnClickListener(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
+        switch (viewId) {
+            case R.id.opn_phonebook:
+                startActivity(new Intent(this, PhoneContacts.class));
+                break;
+            case R.id.opn_customview:
+                startActivity(new Intent(this, CustomViewActivity.class));
+                //Toast.makeText(view.getContext(), "Not done yet :(", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opn_webpage:
+                startActivity(new Intent(this, WebViewActivity.class));
+                break;
+        }
     }
 }
