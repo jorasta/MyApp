2b8628b9ce3f948a5112f88d37acb856b3601e2d
Yevgeni Popov <e.popov@a1.by>
2020-01-24 11:14:09 +0300

    Homework5

---
+++ homework5/src/main/java/by/a1/popov/homework5/MainActivity.java
@@ -0,0 +1,82 @@
+package by.a1.popov.homework5;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ContextBroadcastResiever contextBroadcastResiever;
+    private MyService myService;
+    private TextView textViewLog;
+    private Intent intent;
+    private boolean myBound = false;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            myService = ((MyService.MyBinder) binder).getService();
+            textViewLog.setText(myService.readLogFile());
+            myBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            myBound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewLog = findViewById(R.id.textViewLog);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        intent = new Intent(this, MyService.class);
+
+        contextBroadcastResiever = new ContextBroadcastResiever();
+        registerReceiver(contextBroadcastResiever, intentFilter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            textViewLog.setText(myService.readLogFile());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (!myBound) return;
+        unbindService(serviceConnection);
+        myBound = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (contextBroadcastResiever != null) {
+            unregisterReceiver(contextBroadcastResiever);
+        }
+        if (!myBound) return;
+        unbindService(serviceConnection);
+    }
+}
---
+++ homework5/src/main/java/by/a1/popov/homework5/MyService.java
@@ -0,0 +1,126 @@
+package by.a1.popov.homework5;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MyService extends Service {
+
+    public static final String EXTRA_ACTION = "EXTRA_ACTION";
+    public static boolean bound;
+
+    MyBinder binder = new MyBinder();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        doLogFile(intent.getStringExtra(EXTRA_ACTION));
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void doLogFile(final String action) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // get current time for new log record
+                Date currentTime = Calendar.getInstance().getTime();
+                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy - HH:mm:ss", Locale.getDefault());
+                String strDate = sdf.format(currentTime);
+                // add new log record in file serviceLog.log
+                try {
+                    FileWriter fw = new FileWriter(getFilesDir() + "/serviceLog.log", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter printWriter = new PrintWriter(bw);
+                    printWriter.println(strDate + " - " + action);
+                    printWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                stopSelf();
+            }
+        });
+        thread.start();
+    }
+
+    public String readLogFile() {
+        String filename = "serviceLog.log";
+        String result = null;
+        File file = new File(getFilesDir(), filename);
+        if (file.exists()) {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            FileReader fileReader = null;
+            try {
+                fileReader = new FileReader(file);
+                bufferedReader = new BufferedReader(fileReader);
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+                result = stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (fileReader != null) {
+                    try {
+                        fileReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            result = "FILE NOT FOUND";
+        }
+        return result;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (binder == null) {
+            binder = new MyBinder();
+            bound = true;
+        }
+        return binder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        bound = true;
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        bound = false;
+        return super.onUnbind(intent);
+    }
+
+    class MyBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+}
